3b2985f42adf3ade91347caccb325049acb4c6db
sadineni prasad chowdary <sadineniprasad@hotmail.com>
2021-08-27 09:26:13 -0700

    removing secrets

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id   = "b5a4fb93-1328-4788-8df6-17456bbcb37e"
-  tenant_id         = "03ce3b63-6664-464b-9daf-db8fd75ba351"
-  client_id         = "2e3d282e-0bc1-4780-a6da-25eb387c4549"
-  client_secret     = "HGOi3veeR_xcbNNH3X_ZO~z4XLVpGrpulo"
+  subscription_id   = "subscription"
+  tenant_id         = "TenantID"
+  client_id         = "clientID"
+  client_secret     = "secret"
 }
 
 resource "azurerm_resource_group" "terraformrg" {
